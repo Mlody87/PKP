e6f5fbfa2c470f0c5df2ab6aa687f5cc5792ce1e
Mlody87 <kabatpawel@gmail.com>
2021-09-01 10:08:34 +0200

    Create pkpTest.py

---
+++ tests/pkpTest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium.webdriver import Chrome
+from pages import searchPage
+from pages import resultsPage
+
+@pytest.fixture
+def browser():
+    driver = Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
+    driver.implicitly_wait(15)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+def test_pkp_search(browser):
+    search = searchPage.pkpSearchPage(browser)
+    search.load()
+    search.eatCookies().click()
+    search.closeAdds().click()
+    search.searchConnection("Kraków Główny", "Lausanne")
+
+    results = resultsPage.resultsPage(browser)
+    results.findBestConnection()
+    results.printResults()
